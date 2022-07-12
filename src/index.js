1582777681f6af29a90d63dd647df4f28d8e3855
guardian-ost <guardian.ost@protonmail.com>
2022-07-12 20:31:06 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let config = bracketsConfig.map(item => item.join('')); 
+  while (config.length) {
+    let i = 0;
+    for (let item of config) {
+      if (str.includes(item)) {
+        str = str.replace(item, '');
+      } else {
+        i++;}
+    }
+    if (i === config.length) {
+      config.length = 0;
+    }  
+  }
+  return !str;
+};
+
